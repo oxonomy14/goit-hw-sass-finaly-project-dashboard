6387e72f122f2b480430768e6a8db0df16a8a4a3
oxonomy14 <oxonomy14@gmail.com>
2026-01-15 10:48:48 +0000

    Deploying to gh-pages from @ oxonomy14/goit-hw-sass-finaly-project-dashboard@5aa4b4fc21b0a3e53d40a403515e7c8bafa8e2bc 🚀

--- assets/index-f2690ea5.js
+++ assets/index-f2690ea5.js
@@ -39,4 +39,25 @@
     fetch(link.href, fetchOpts);
   }
 })();
-const style = "";
+const main = "";
+const refsMenu = {
+  openMenuBtn: document.querySelector(".js-menu-open"),
+  closeMenuBtn: document.querySelector(".js-menu-close"),
+  overlayMenu: document.querySelector(".js-menu")
+};
+const toggleMenu = () => {
+  const isMenuOpen = refsMenu.openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+  refsMenu.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+  refsMenu.overlayMenu.classList.toggle("is-open");
+  const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
+  bodyScrollLock[scrollLockMethod](document.body);
+};
+refsMenu.openMenuBtn.addEventListener("click", toggleMenu);
+refsMenu.closeMenuBtn.addEventListener("click", toggleMenu);
+window.matchMedia("(min-width: 1200px)").addEventListener("change", (event) => {
+  if (!event.matches)
+    return;
+  refsMenu.overlayMenu.classList.remove("is-open");
+  refsMenu.openMenuBtn.setAttribute("aria-expanded", false);
+  bodyScrollLock.enableBodyScroll(document.body);
+});
